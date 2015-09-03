e8b8598084ca26937246641621cc5b80e873a69d
Matt <matt@quru.com>
2015-09-03 15:52:45 +0100

    Bump version to match branch

--- src/imageserver/__about__.py
+++ src/imageserver/__about__.py
@@ -30,7 +30,7 @@ __summary__ = "A high performance dynamic image server"
 __uri__ = "https://quru.com/qis/"
 __source_uri__ = "https://github.com/quru/qis"
 
-__version__ = "1.33"
+__version__ = "2.0.0-dev-0"
 
 __author__ = "Quru Ltd"
 __email__ = "info@quru.com"
