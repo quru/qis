16cc55a0d081dff67525a2d643c4e51772d11e89
Matt <matt@quru.com>
2017-01-23 13:00:52 +0000

    Updated Docker deployment for Docker 1.12 and Docker-Compose

--- deploy/docker/docker-1.0-obsolete/README.md
+++
@@ -1,3 +0,0 @@
-# Obsolete Docker Images
-
-These files were developed for Docker 1.0 and are now obsolete.
--- deploy/docker/postgres/run-postgres.sh
+++ deploy/docker/postgres/run-postgres.sh
@@ -6,9 +6,29 @@
 
 PG_BIN_DIR=/usr/lib/postgresql/9.5/bin
 PG_CONF_FILE=$PGDATA/postgresql.conf
-FIRST_RUN_LOG_FILE=$PGDATA/pg_log/first_run.log
+FIRST_RUN_FILE=$PGDATA/first_run.log
 
-if [ ! -f "$FIRST_RUN_LOG_FILE" ]; then
+# Check that the PGDATA directory is accessible from the container
+if [ ! -w $PGDATA ]; then
+  echo "The PGDATA directory is not writable for the container user $USER."
+  echo "Ensure that the path exists as a volume or host-mounted directory"
+  echo "and has write permissions for the container user."
+  echo $PGDATA
+  exit 1
+fi
+
+# On first run, create the Postgres "cluster"
+if [ ! -f $PG_CONF_FILE ]; then
+
+	echo "Initialising new data directory at $PGDATA"
+	$PG_BIN_DIR/initdb --locale=en_GB.UTF-8
+
+	# Require passwords for external connections
+	echo "host    all             all             0.0.0.0/0               md5" >> $PGDATA/pg_hba.conf
+
+	# Enable external TCP connections, set logging to stderr for Docker to pick up
+	echo "listen_addresses = '*'" >> $PG_CONF_FILE && \
+	echo "log_destination = 'stderr'" >> $PG_CONF_FILE
 
 	# Tune postgres.conf
 	echo "max_connections = $PG_MAX_CONNECTIONS" >> $PG_CONF_FILE
@@ -20,17 +40,20 @@ if [ ! -f "$FIRST_RUN_LOG_FILE" ]; then
 	#echo "log_filename = 'postgresql-%a.log'" >> $PG_CONF_FILE
 	#echo "log_truncate_on_rotation = on" >> $PG_CONF_FILE
 	#echo "log_rotation_age = 1d" >> $PG_CONF_FILE
+fi
 
-	# Start Postgres in the background
-	mkdir $PGDATA/pg_log && chmod 700 $PGDATA/pg_log
-	$PG_BIN_DIR/pg_ctl start -w -l $FIRST_RUN_LOG_FILE
+# On first run, create the database
+if [ ! -f "$FIRST_RUN_FILE" ]; then
 
-	# Create the QIS user and database
+	echo "Starting background Postgres to create the database"
+	$PG_BIN_DIR/pg_ctl start -w -l $FIRST_RUN_FILE
+
+	echo "Creating the QIS database role and new empty database"
 	psql -h localhost -U postgres -d postgres -c "CREATE ROLE \"$PG_USER\" WITH LOGIN PASSWORD '$PG_PASSWORD'"
 	createdb -h localhost -U postgres --owner $PG_USER qis-cache
 	createdb -h localhost -U postgres --owner $PG_USER qis-mgmt
 
-	# Stop background process, we now need to run it foreground
+	echo "Stopping background Postgres, about to start normally"
 	$PG_BIN_DIR/pg_ctl stop -w
 fi
 
