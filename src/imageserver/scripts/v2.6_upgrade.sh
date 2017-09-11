b56fa0a84dd4ef227384551e282ed7e401fe605d
Matt <matt@quru.com>
2017-09-11 17:08:51 +0100

    Search/Replace, you've gone too far this time

--- src/imageserver/scripts/v2.6_upgrade.sh
+++ src/imageserver/scripts/v2.6_upgrade.sh
@@ -11,21 +11,21 @@ echo "Changing directory to $QIS_HOME/src/imageserver/static/js"
 cd "$QIS_HOME/src/imageserver/static/js"
 
 echo "Removing old Javascript files"
-rm `find . -name '*.min.js'`
+rm `find . -name '*_yc.js'`
 
 read -p "Do you need to maintain compatibility of Javascript files with earlier versions of QIS? [y/n]" RESP
 
 if [[ "$RESP" == "y" ]]; then
     echo "Creating compatibility symlinks"
-    ln -s canvas_view.min.js canvas_view.min.js
-    ln -s excanvas.min.js excanvas.min.js
-    ln -s gallery_view.min.js gallery_view.min.js
-    ln -s simple_view.min.js simple_view.min.js
-    ln -s slideshow_view.min.js slideshow_view.min.js
-    ln -s lib/mootools.min.js mootools.min.js
-    
+    ln -s canvas_view.min.js canvas_view_yc.js
+    ln -s excanvas.min.js excanvas_yc.js
+    ln -s gallery_view.min.js gallery_view_yc.js
+    ln -s simple_view.min.js simple_view_yc.js
+    ln -s slideshow_view.min.js slideshow_view_yc.js
+    ln -s lib/mootools.min.js mootools_yc.js
+
     echo "The following symlinks can be deleted after your URLs and file references have been updated:"
-    find . -name '*.min.js'
+    find . -name '*_yc.js'
 fi
 
 echo ""
