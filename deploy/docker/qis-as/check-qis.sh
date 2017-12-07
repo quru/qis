11516fab561866afc33eba47a146e566db8f05f6
Matt <matt@quru.com>
2017-12-07 15:00:00 +0000

    Missed one from d85039e, also use a better check

--- deploy/docker/qis-as/check-qis.sh
+++ deploy/docker/qis-as/check-qis.sh
@@ -2,4 +2,4 @@
 
 # Runs a simple check to test that the QIS service is running
 
-curl -A "curl/docker healthcheck" -s "http://localhost/api/v1/list?path=test_images" | grep '"status": 200' || exit 1
+curl -A "curl/docker healthcheck" -s "http://localhost/api/v1/list/?path=/&limit=1" | grep '"status": 200' || exit 1
