624a468c4ff01d3b18928d87be5a905f5348f553
Matt <matt@quru.com>
2018-10-29 14:46:27 +0000

    csrf.py is not compatible with itsdangerous v1.x (installed for Flask)

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     install_requires=[
         "Pillow==5.2.0",
         "Flask==1.0.2",
+        "itsdangerous<1",
         "python-ldap==3.1.0",
         "pylibmc==1.5.2",
         "psycopg2==2.7.5",
